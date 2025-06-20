733ba50355890fc22c7b4258f6920807fbc1063d
kwan wynn <60003997+mintkw@users.noreply.github.com>
2025-06-20 10:27:05 +0100

    Experimental version of a stages to sequence function that adaptively handles different normal and abnormal levels and scales

--- stages_to_sequence.py
+++ stages_to_sequence.py
@@ -13,20 +13,63 @@ from evaluation import evaluate_autoencoder, evaluate_sequence
 def stages_to_sequence(num_biomarkers, dataloader, net, device):
     # Infer underlying sequence from latent stage predictions.
     # By this formula, a later score implies earlier abnormality and thus earlier position.
-    # The problem is it assumes an identical normal and abnormal value for all biomarkers.
-    # Maybe first obtain biomarker values at the min and max stages to rescale into 0 normal and 1 abnormal?
+    # But it still depends on the direction of the learned latent...
     biomarker_scores = torch.zeros(num_biomarkers, device=device, requires_grad=False)
+
+    start = torch.zeros(num_biomarkers, device=device, requires_grad=False)  # estimate of biomarker levels at the start
+    end = torch.zeros(num_biomarkers, device=device, requires_grad=False)  # estimate of biomarker levels at the end
+
+    start_count = 0
+    end_count = 0
 
     for X, _ in dataloader:
         X = X.to(device)
         pred = net.encode(X)
+
+        # consider measurements predicted to be within stages 0 to 3 and num_biomarkers - 3 to num_biomarkers?
+        idx_within_start = torch.where(pred < 3 / num_biomarkers)[0]  # extract single tensor from tuple
+        idx_within_end = torch.where(pred > 1 - (3 / num_biomarkers))[0]
+
+        start_count += idx_within_start.shape[0]
+        end_count += idx_within_end.shape[0]
+
+        if start_count > 0:
+            start = start * (start_count - idx_within_start.shape[0]) / start_count \
+                + X[idx_within_start].sum(0) / start_count
+        if end_count > 0:
+            end = end * (end_count - idx_within_end.shape[0]) / end_count \
+                + X[idx_within_end].sum(0) / end_count
+
+    reverse = torch.ones(num_biomarkers).to(device)
+    reverse[torch.where(start > end)] = -1  # indicating that the level decreases with abnormality
+
+    midpoints = start + (end - start) / 2
+
+    corrected_start = torch.min(start, end)
+    corrected_end = torch.max(start, end)
+
+    for X, labels in dataloader:
+        X = X.to(device)
+        pred = net.encode(X)
+
+        # Scale X using prediction and start/end.
+        # first flip around the midpoints so start < end
+        X = midpoints + reverse * X - midpoints
+
+        # then clip
+        X = torch.max(X, corrected_start)
+        X = torch.min(X, corrected_end)
+
+        # rescale
+        X = (X - corrected_start) / (corrected_end - corrected_start)
+
         biomarker_scores += (pred * X).sum(dim=0)
 
     return torch.argsort(biomarker_scores, descending=True)
 
 
 if __name__ == "__main__":
-    dataset_name = "synthetic_1200_50_dpm_0"
+    dataset_name = "synthetic_120_10_dpm_0"
 
     # Load dataset
     dataset = SyntheticDatasetVec(dataset_name=dataset_name)
@@ -59,7 +102,7 @@ if __name__ == "__main__":
     vae_dec.load_state_dict(torch.load(dec_model_path, map_location=DEVICE))
 
     # # delete later. verify that everything loaded alright
-    # print("Mean squared error:", evaluate_autoencoder(dataloader, vae, DEVICE)[0])
+    print("Mean squared error:", evaluate_autoencoder(dataloader, vae, DEVICE)[0])
 
     # Infer sequence from stage information
     vae_seq_prediction = stages_to_sequence(num_biomarkers, dataloader, vae, DEVICE)
@@ -82,7 +125,7 @@ if __name__ == "__main__":
     ae_dec.load_state_dict(torch.load(dec_model_path, map_location=DEVICE))
 
     # # delete later. verify that everything loaded alright
-    # print("Mean squared error:", evaluate_autoencoder(dataloader, ae, DEVICE)[0])
+    print("Mean squared error:", evaluate_autoencoder(dataloader, ae, DEVICE)[0])
 
     # Infer sequence from stage information
     ae_seq_prediction = stages_to_sequence(num_biomarkers, dataloader, ae, DEVICE)
